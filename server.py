28cd680a802ef775cf886fca572ee468eaa81a7a
RaviTejaKomma <ravieee929374s@gmail.com>
2018-04-05 17:11:56 +0530

    added code for testing

--- server.py
+++ server.py
@@ -92,6 +92,7 @@ bot = telepot.Bot("585184839:AAGaTVTymWCTEwk3xTOYL-QDAwo8jNonUkk")
 url = "https://api.telegram.org/bot585184839:AAGaTVTymWCTEwk3xTOYL-QDAwo8jNonUkk/sendPhoto";
 
 def sendImage(filename):
+    filename = "object_detection.png"
     print("Entered function : ",filename)
     files = {'photo': open(filename, 'rb')}
     data = {'chat_id' : "460626793"}
