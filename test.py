2b64734364fd4d2d63631ef4d25e40d48d9a9c89
RaviTejaKomma <ravieee929374s@gmail.com>
2018-04-05 16:46:17 +0530

    added print statements for debugging

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ bot = telepot.Bot("585184839:AAGaTVTymWCTEwk3xTOYL-QDAwo8jNonUkk")
 url = "https://api.telegram.org/bot585184839:AAGaTVTymWCTEwk3xTOYL-QDAwo8jNonUkk/sendPhoto";
 
 def sendImage(filename):
+    print("Entered function")
     files = {'photo': open(filename, 'rb')}
     data = {'chat_id' : "460626793"}
     text_data = "Person Detected"
@@ -14,5 +15,5 @@ def sendImage(filename):
     print(r)
 
 if __name__ == "__main__":
-	filename = input("Enter the path : ")
+    filename = input("Enter the path : ")
     sendImage(filename)
