5ef9695982638f25288ddaf10d7c49daaa9c63ef
RaviTejaKomma <ravieee929374s@gmail.com>
2018-04-05 16:42:41 +0530

    added test file

---
+++ test.py
@@ -0,0 +1,18 @@
+import telepot
+from telepot.loop import MessageLoop
+import requests 
+
+bot = telepot.Bot("585184839:AAGaTVTymWCTEwk3xTOYL-QDAwo8jNonUkk")
+url = "https://api.telegram.org/bot585184839:AAGaTVTymWCTEwk3xTOYL-QDAwo8jNonUkk/sendPhoto";
+
+def sendImage(filename):
+    files = {'photo': open(filename, 'rb')}
+    data = {'chat_id' : "460626793"}
+    text_data = "Person Detected"
+    bot.sendMessage(data['chat_id'], text=text_data)
+    r= requests.post(url, files=files, data=data)
+    print(r)
+
+if __name__ == "__main__":
+	filename = input("Enter the path : ")
+    sendImage(filename)
